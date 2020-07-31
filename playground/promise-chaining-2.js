4dd26299931d60741540d2b6a643a75eb9b39fc0
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-07-31 22:29:24 +0700

    integrating async/await into code

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5f241de1c5462a452826e67a').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5f241de1c5462a452826e67a').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5f22fcbeb787680ad024690f').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
