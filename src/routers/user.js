800ad69541f172635f260726d1d24018327b1aaf
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-08-01 22:05:53 +0700

    using bcryptjs for storing password + mongoose middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const { update } = require('../models/user')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -48,7 +49,13 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+
+        user.name = 'something else'
+
+        updates.forEach((update) =>  user[update] = req.body[update])
+        await user.save()
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
         
         if (!user) {
             return res.status(404).send()
