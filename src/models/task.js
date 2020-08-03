9acab7e708f477d36be8a5b23f843c8d1332a2b3
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-08-03 21:21:16 +0700

    add filtering data (tasks)

--- src/models/task.js
+++ src/models/task.js
@@ -19,24 +19,6 @@ const taskSchema = new mongoose.Schema({
     timestamps: true
 })
 
-
 const Task = mongoose.model('Task', taskSchema)
-
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String,
-//         trim: true,
-//         required: true
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false
-//     },
-//     owner: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         required: true,
-//         ref: 'User'
-//     }
-// })
 
 module.exports = Task
