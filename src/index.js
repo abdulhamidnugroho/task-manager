6bfbfbcffee26436223b7418f5d04344aa4540ae
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-08-03 20:49:48 +0700

    Auth Task Endpoints

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bcrypt = require('bcryptjs')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -23,8 +22,21 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('5f268bed8ce84e36c0444332')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+    const user = await User.findById('5f268b26bf9bd636a8f3af55')
+    await user.populate('tasks').execPopulate()
+    // console.log(user.tasks)
+
+}
+
+main()
--- src/models/task.js
+++ src/models/task.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 const Task = mongoose.model('Task', {
     description: {
         type: String,
         trim: true,
-        required: true,
+        required: true
     },
     completed: {
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
-})
-
-const task = new Task({
-    description: 'This is description',
-    completed: true
 })
 
 module.exports = Task
