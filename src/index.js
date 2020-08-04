031e1a3c383e72c20e4aa437c1be12fdd9decd10
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-08-04 21:32:45 +0700

    validating file uploads

--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Pls upload a word file'))
+        }
+
+        cb(undefined, true)
+        // cb(new Error('file must be PDF'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+    }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
