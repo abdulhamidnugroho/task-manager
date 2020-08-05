3a05dcec07833cc6357162acb9be56a6380ea559
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-08-05 19:51:35 +0700

    validating file uploads (users)

--- src/routers/user.js
+++ src/routers/user.js
@@ -97,7 +97,17 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Pls upload an image file'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
