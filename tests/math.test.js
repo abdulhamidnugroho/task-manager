781786ec204ab4a03025213c119eba19baf958c8
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2020-08-12 21:33:22 +0700

    test and asssertions

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,12 @@
-test('Hello World', () => {
+const { calculateTip } = require('../src/math')
+const { TestScheduler } = require('jest')
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+     expect(total).toBe(13)
 })
 
-test('This should fail', () => {
-    throw new Error('failure')
+test('Should Calculate Total with default tip', () => {
+    const total = calculateTip(10)
+     expect(total).toBe(12.5)
 })
