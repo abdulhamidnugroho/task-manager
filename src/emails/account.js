b78a95c7abdf08bd4a4d38d938ee2a9854c70066
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2020-09-06 20:59:57 +0700

    mocking libraries

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,23 +1,22 @@
 const sgMail = require('@sendgrid/mail')
-const mail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
-        to: 'ahamidnugroho@gmail.com',
+        to: email,
         from: 'abdulhamidnugroho@gmail.com',
         subject: 'Thanks for sign up',
-        text: `Welcome to the app, $(name) . Let me know how you get along with the app`
+        text: `Welcome to the app, ${name} . Let me know how you get along with the app`
     })
 }
 
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
-        to: mail,
+        to: email,
         from: 'abdulhamidnugroho@gmail.com',
         subject: 'Sorry to see you go',
-        text: `Bye, I hope to see you back again soon`
+        text: `Bye ${name} , I hope to see you back again soon`
     })
 }
 
